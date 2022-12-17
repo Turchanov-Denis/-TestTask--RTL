96721df35eca9ce2e6a2411b41cda12d83d35024
Turchanov-Denis <96124363+Turchanov-Denis@users.noreply.github.com>
2022-12-17 16:03:52 +0200

    add style: descr + search

--- src/layout/InventoryPage.vue
+++ src/layout/InventoryPage.vue
@@ -26,7 +26,12 @@ const inventory = useInventoryStore();
       <div class="inventory"></div>
     </div>
     <div class="inventory-section__row">
-      <div class="search"></div>
+      <div class="search">
+        <input type="text" disabled />
+        <button class="search__close">
+          <img src="../assets/InventoryIcon/carbon_close.png" alt="" />
+        </button>
+      </div>
     </div>
   </section>
 </template>
@@ -111,9 +116,11 @@ export default {};
 }
 
 .search {
-    margin-top: 20px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
   box-sizing: border-box;
-  position: absolute;
   width: 785px;
   height: 72px;
   background: #262626;
@@ -121,5 +128,23 @@ export default {};
 
   border: 1px solid #4d4d4d;
   border-radius: 12px;
+  input {
+    margin-left: 18px;
+    width: 699px;
+    height: 36px;
+    background: linear-gradient(
+      90deg,
+      #3c3c3c 0%,
+      #444444 51.04%,
+      #333333 100%
+    );
+    border-radius: 12px;
+  }
+  &__close {
+    position: absolute;
+    top: 6px;
+    right: 5px;
+    background-color: rgba(255, 255, 255, 0);
+  }
 }
 </style>
