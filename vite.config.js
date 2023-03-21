3e2b28647e69f3c8f42e81637ad5ee50f6ef2800
Turchanov-Denis <turchanovdenis786@gmail.com>
2023-03-21 18:55:42 +0200

    init

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://turchanov-denis.github.io/-TestTask--RTL/",
   plugins: [vue()],
   resolve: {
     alias: {
